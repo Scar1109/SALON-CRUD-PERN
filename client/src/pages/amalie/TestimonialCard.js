34a0d56205c04b2de5a73968fc5a6c78b392d9b9
Kaveen Dimantha <kaveendimantha@gmail.com>
2024-09-07 21:09:02 +0530

    SCP | Design Testimonial Card

--- client/src/pages/amalie/TestimonialCard.js
+++ client/src/pages/amalie/TestimonialCard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Rate } from 'antd';
 import logo from "../../images/logo.jpeg";
 
 function TestimonialCard() {
+
     return (
         <div className="flex border border-gray-300 rounded-lg overflow-hidden shadow-lg">
             {/* Image Section */}
@@ -11,12 +13,18 @@ function TestimonialCard() {
                 alt="Testimonial image"
             />
             {/* Text Section */}
-            <div className="flex-1 p-4">
-                <p className="text-gray-700">
-                    "This is a testimonial text that highlights the service or
-                    product experience. It's an example of how users might
-                    review or comment on the service."
-                </p>
+            <div className="flex-1 ml-1 mt-2">
+                <div className="flex flex-row ml-[-7]">
+                    <img class="w-10 h-10 rounded-full" src="https://cdn-icons-png.flaticon.com/256/3135/3135768.png" alt="Rounded avatar"></img>
+                    <div className="flex flex-col">
+                    <h className="ml-2 mt-1 text-base font-bold">Reviewer Name</h>
+                    <p className="ml-3">join date</p>
+                    </div>
+                    <div className="ml-2 mt-2">
+                        <Rate disabled defaultValue={2} />
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
